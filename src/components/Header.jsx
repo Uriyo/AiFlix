39fd30319b5e7ec4fa2a729b97931b788d86e514
Kushagra Gangwar <kgang6434@gmail.com>
2024-02-26 17:50:09 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+  return (
+    <div>
+      <img src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" alt="logo"
+        width="250" height="100"  
+      />
+      Header
+    </div>
+  )
+}
+
+export default Header
